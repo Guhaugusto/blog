00b45a3b035d4ba7dcf4deb4501706c23f7f1c7c
Guhaugusto <gustavogvfontes2004@gmail.com>
2024-08-19 12:56:47 -0300

    else e if

--- helpers.php
+++ helpers.php
@@ -1,12 +1,22 @@
-<?php 
+<?php
 
-function saudacao() 
+function saudacao(): string
 {
-    return '<h1>Boa tarde</h1>';
-}
-function resumirTexto($texto, $limite, $continue = '...')
-{
-   return $texto;   
-}
+    $hora = 15;
 
-?>
+    if ($hora >= 0 && $hora <= 5) {
+        $saldacao = '<h1>Boa madrugada</h1>';
+    } elseif ($hora >= 6 and $hora <= 12) {
+        $saldacao = '<h1>Bom dia</h1>';
+    } elseif ($hora >= 13 and $hora <= 18) {
+        $saldacao = '<h1>Boa tarde</h1>';
+    } else {
+        $saldacao = '<h1>Boa noite</h1>';
+    }
+
+    return $saldacao;
+}
+function resumirTexto(string $texto,  int $limite, string $continue = '...'): string
+{
+    return $texto;
+}
--- index..php
+++ index..php
@@ -1,12 +1,12 @@
-
     <?php 
+    // declare (strict_types = 1);
+
     require_once'sistema/configuracao.php';
     include_once'helpers.php';
 
-    $texto = 'texto para resumir vindo de uma variavel';
+    $texto = 'Texto para resumir';
 
-    echo saudacao();
+
     echo '<hr>';
-    echo resumirTexto($texto, 50);
-    
-    ?>
+    echo saudacao();
+?>
